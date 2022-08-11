27cb497cb774f673d5b6a65917091170c3e2e157
Elocinacademia <nicolezhanacademia@gmail.com>
2022-08-11 10:36:37 +0100

    Added decision tree model

--- code/src/helpers/pre_data.py
+++ code/src/helpers/pre_data.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
 
-train = pd.read_csv("../../../data/data.csv")
+df = pd.read_csv("../../../data/data.csv")
 print("Read data ....")
 
 #Drop nun values
-train.dropna(inplace=False)
 
+df.dropna(inplace=True)
+df = pd.get_dummies(df)
+
+#Split the dataset
+sum_n = len(df)
+cut_n = round(sum_n * 0.8)
+
+df[:cut_n].to_csv("../../../data/train.csv",index=False)# 不保存行索引
+df[cut_n:].to_csv("../../../data/test.csv",index=False)
+
+
+import pdb; pdb.set_trace()
